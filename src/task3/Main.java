3800e979cfb8264f6042122616564177dbca85e1
Anton Sayutin <sayutosss@gmail.com>
2021-02-23 19:27:24 +0300

    ц

---
+++ src/task3/Main.java
@@ -0,0 +1,16 @@
+package task3;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Locale;
+
+public class Main {
+    public static void main(String[] args) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(1993 , 04, 30);
+
+    }
+    
+}
